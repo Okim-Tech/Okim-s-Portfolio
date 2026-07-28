fc18f4a1f1f998b4304aa52d9a21b31638632404
Okim-Tech <okimpeace187@gmail.com>
2026-07-28 08:17:00 +0100

    Build navigation bar and added okim brand logo to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import "./Navbar.css";
+import logoImg from "../../assets/images/logo.jpg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={logoImg} alt="Okim logo" className="logo-img" />
+        </div>
+
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+
+            <li>
+              <a href="#about">About</a>
+            </li>
+
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+
+            <li>
+              <a href="#services">Services</a>
+            </li>
+
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button className="hire-btn">Hire Me</button>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Project</div>;
+  return <div></div>;
 };
 
 export default Projects;
